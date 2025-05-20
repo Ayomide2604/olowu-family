3bfb3e4bd281adb78a42a6acc4240dc227292f60
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-05-20 19:43:05 +0100

    Update Sidebar component to include Instagram and TikTok icons, and improve menu interaction

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaTimes, FaBars } from "react-icons/fa";
+import { FaTimes, FaBars, FaTiktok, FaInstagram } from "react-icons/fa";
 
 const Sidebar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -21,7 +21,7 @@ const Sidebar = () => {
 					isMenuOpen ? "offcanvas-menu" : ""
 				}`}
 			>
-				<div className="site-mobile-menu-header">
+				<div className="site-mobile-menu-header ">
 					<div className="site-mobile-menu-close mt-3">
 						<span className="icon-close2 js-menu-toggle" onClick={toggleMenu}>
 							<FaTimes color="black" />
@@ -29,7 +29,7 @@ const Sidebar = () => {
 					</div>
 				</div>
 				<div className="site-mobile-menu-body">
-					<ul className="site-nav-wrap">
+					<ul className="site-nav-wrap" onClick={toggleMenu}>
 						<li>
 							<a href="#section-home" className="nav-link">
 								Home
@@ -40,24 +40,20 @@ const Sidebar = () => {
 								Photos
 							</a>
 						</li>
+
 						<li>
-							<a href="#section-about" className="nav-link">
-								About
+							<a href="#">
+								<span className="icon-instagram">
+									<FaInstagram />
+								</span>
 							</a>
 						</li>
+
 						<li>
-							<a href="#section-testimonial" className="nav-link">
-								Testimonial
-							</a>
-						</li>
-						<li>
-							<a href="#section-blog" className="nav-link">
-								Blog
-							</a>
-						</li>
-						<li>
-							<a href="#section-contact" className="nav-link">
-								Contact
+							<a href="#">
+								<span className="icon-tiktok">
+									<FaTiktok />
+								</span>
 							</a>
 						</li>
 					</ul>
@@ -117,25 +113,20 @@ const Sidebar = () => {
 							</a>
 						</li>
 					</ul>
-					<ul className="social js-clone-nav">
+					<ul className="social js-clone-nav d-flex justify-content-center">
 						<li>
 							<a href="#">
-								<span className="icon-instagram"></span>
+								<span className="icon-instagram">
+									<FaInstagram />
+								</span>
 							</a>
 						</li>
+
 						<li>
 							<a href="#">
-								<span className="icon-facebook"></span>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span className="icon-twitter"></span>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<span className="icon-linkedin"></span>
+								<span className="icon-tiktok">
+									<FaTiktok />
+								</span>
 							</a>
 						</li>
 					</ul>
